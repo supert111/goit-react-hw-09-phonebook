bbf2104d7de0aeb1447356f7747fc4166ee4d346
supert111 <sportsmen@bigmir.net>
2021-06-18 11:03:06 +0300

    finish

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,31 +7,37 @@ import { addContact } from '../../redux/contacts';
 const nameInputId = shortid.generate();
 const phoneInputId = shortid.generate();
 
+const initialState = {name: '', number: ''}
+
 export default function ContactForm () { 
-    const [name, setName] = useState('');
-    const [number, setNumber] = useState('');
+    const [userContact, setUserContact] = useState(initialState);
+    // const [name, setName] = useState('');
+    // const [number, setNumber] = useState('');
     
     const dispatch = useDispatch();
     
     const handleChange = (element) => {
         const { name, value } = element.target;
 
-        switch (name) {
-            case 'name': setName(value);
-                break;
-            case 'number': setNumber(value);
-                break;
-            default: console.warn(`Тип поля name - ${name} не обрабатывается`);
-        }
+        setUserContact(prev => ({...prev, [name]: value}));
+        // switch (name) {
+        //     case 'name': setName(value);
+        //         break;
+        //     case 'number': setNumber(value);
+        //         break;
+        //     default: console.warn(`Тип поля name - ${name} не обрабатывается`);
+        // }
     };
 
     const handleSubmit = (el) => {
         el.preventDefault();
         
-        dispatch(addContact({name, number}));
+        dispatch(addContact(userContact));
+        //dispatch(addContact({name, number}));
 
-        setName('');
-        setNumber('');
+        setUserContact(initialState);
+        // setName('');
+        // setNumber('');
     }
 
         return (           
@@ -40,7 +46,7 @@ export default function ContactForm () {
                         <input className={styles.input_display}
                             type="text"
                             name="name"
-                            value={name}
+                            value={userContact.name}
                             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
                             required
@@ -52,7 +58,7 @@ export default function ContactForm () {
                         <input className={styles.input_display}
                             type="tel"
                             name="number"
-                            value={number}
+                            value={userContact.number}
                             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
                             required

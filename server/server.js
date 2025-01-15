6ccb17f7ff6a2b836c6b564525db6da108240960
Mykola Saulko <saulkomyk@gmail.com>
2025-01-15 23:20:07 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Phonebook API running');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

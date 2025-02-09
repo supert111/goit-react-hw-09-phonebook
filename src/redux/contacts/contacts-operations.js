4fe29035cd5d41e45eb786bf103312b3afa015f2
Mykola Saulko <saulkomyk@gmail.com>
2025-02-09 21:10:42 +0200

    Update contacts-operations.js

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -53,46 +53,114 @@
 
 
 /////////////////////////////////////////////////////////////////////////
+// import axios from 'axios';
+// import {
+//     fetchContactRequest,
+//     fetchContactSuccess,
+//     fetchContactError,
+//     addContactSuccess,
+//     addContactRequest,
+//     addContactError,
+//     deleteContactRequest,
+//     deleteContactSuccess,
+//     deleteContactError,
+// } from './contacts-actions';
+
+// //axios.defaults.baseURL = 'http://localhost:4040';
+
+// export const fetchContacts = () => dispatch => {
+//     dispatch(fetchContactRequest());
+
+//     axios
+//         .get('/contacts')
+//         .then(({data}) => dispatch(fetchContactSuccess(data)))
+//         .catch(error => dispatch(fetchContactError(error.message)));
+// };
+
+// export const addContact = ({name, number}) => dispatch => {
+//     const contact = {name, number};
+
+//     dispatch(addContactRequest());
+
+//     axios
+//         .post('/contacts', contact)
+//         .then(({data}) => dispatch(addContactSuccess(data)))
+//         .catch(error => dispatch(addContactError(error.message)));
+// }
+
+// export const deleteContact = contactId => dispatch => {
+//     dispatch(deleteContactRequest());
+
+//     axios
+//         .delete(`/contacts/${contactId}`)
+//         .then(() => dispatch(deleteContactSuccess(contactId)))
+//         .catch(error => dispatch(deleteContactError(error.message)));
+// };
+
+
 import axios from 'axios';
 import {
-    fetchContactRequest,
-    fetchContactSuccess,
-    fetchContactError,
-    addContactSuccess,
-    addContactRequest,
-    addContactError,
-    deleteContactRequest,
-    deleteContactSuccess,
-    deleteContactError,
+  fetchContactRequest,
+  fetchContactSuccess,
+  fetchContactError,
+  addContactSuccess,
+  addContactRequest,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
 } from './contacts-actions';
 
-//axios.defaults.baseURL = 'http://localhost:4040';
+// Add axios interceptor at the top of the file
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (
+      error.response?.status === 500 &&
+      error.response?.data?.includes('jwt expired')
+    ) {
+      // Redirect to login page
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
 
 export const fetchContacts = () => dispatch => {
-    dispatch(fetchContactRequest());
+  dispatch(fetchContactRequest());
 
-    axios
-        .get('/contacts')
-        .then(({data}) => dispatch(fetchContactSuccess(data)))
-        .catch(error => dispatch(fetchContactError(error.message)));
+  axios
+    .get('/contacts')
+    .then(({ data }) => dispatch(fetchContactSuccess(data)))
+    .catch(error => dispatch(fetchContactError(error.message)));
 };
 
-export const addContact = ({name, number}) => dispatch => {
-    const contact = {name, number};
+export const addContact =
+  ({ name, number }) =>
+  dispatch => {
+    const contact = { name, number };
 
     dispatch(addContactRequest());
 
     axios
-        .post('/contacts', contact)
-        .then(({data}) => dispatch(addContactSuccess(data)))
-        .catch(error => dispatch(addContactError(error.message)));
-}
+      .post('/contacts', contact)
+      .then(({ data }) => dispatch(addContactSuccess(data)))
+      .catch(error => dispatch(addContactError(error.message)));
+  };
 
 export const deleteContact = contactId => dispatch => {
-    dispatch(deleteContactRequest());
+  dispatch(deleteContactRequest());
 
-    axios
-        .delete(`/contacts/${contactId}`)
-        .then(() => dispatch(deleteContactSuccess(contactId)))
-        .catch(error => dispatch(deleteContactError(error.message)));
+  axios
+    .delete(`/contacts/${contactId}`)
+    .then(() => dispatch(deleteContactSuccess(contactId)))
+    .catch(error => {
+      if (
+        error.response?.status === 500 &&
+        error.response?.data?.includes('jwt expired')
+      ) {
+        window.location.href = '/login';
+      }
+      dispatch(deleteContactError(error.message));
+    });
 };
